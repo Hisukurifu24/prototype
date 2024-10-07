26db4c0c65164992a7d9ffb5d1e09a5f97de46a0
Francesco Naletto <46630982+Hisukurifu24@users.noreply.github.com>
2024-10-07 16:52:12 +0200

    deploy on firebase

--- front-end-prototype/src/app/app.config.ts
+++ front-end-prototype/src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding()), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding()), provideHttpClient(), provideFirebaseApp(() => initializeApp({"projectId":"tminiapp-unipd","appId":"1:430071646784:web:a9ec78539b8661b40dada5","storageBucket":"tminiapp-unipd.appspot.com","apiKey":"AIzaSyAlibrFDd6upVY8wAliZS4GCdNP14FDu-E","authDomain":"tminiapp-unipd.firebaseapp.com","messagingSenderId":"430071646784","measurementId":"G-WRMNP6THR5"})), provideAnalytics(() => getAnalytics()), ScreenTrackingService]
 };
