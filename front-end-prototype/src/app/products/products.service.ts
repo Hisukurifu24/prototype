8bd685adad88cf6e5dd74a7eb773d4870e8a8bb6
Francesco Naletto <46630982+Hisukurifu24@users.noreply.github.com>
2024-10-24 15:24:34 +0200

    Fix product service

--- front-end-prototype/src/app/products/products.service.ts
+++ front-end-prototype/src/app/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Product } from './product.model';
 import { dummyProducts } from './dummyProducts';
 
@@ -6,38 +6,41 @@ import { dummyProducts } from './dummyProducts';
   providedIn: 'root'
 })
 export class ProductsService {
-  private allProducts: Product[] = dummyProducts;
+  private allProducts = signal<Product[]>(dummyProducts);
+
+  constructor() {
+    this.allProducts.set(dummyProducts);
+    this.loadProducts();
+
+    effect(() => {
+      this.saveProducts();
+    });
+  }
 
   getProduct(id: number): Product {
-    this.loadProducts();
-
-    return this.allProducts.find(p => p.id == id) as Product;
+    return this.allProducts().find(p => p.id == id) as Product;
   }
   getProducts(): Product[] {
-    this.loadProducts();
-    return this.allProducts;
+    return this.allProducts();
   }
   deleteProduct(p: Product): boolean {
-    if (this.allProducts.find(pr => pr.id == p.id)) {
-      this.allProducts = this.allProducts.filter(pr => pr.id != p.id);
-      this.saveProducts();
+    if (this.allProducts().find(pr => pr.id == p.id)) {
+      this.allProducts.set(this.allProducts().filter(pr => pr.id != p.id));
       return true;
     } else {
       return false;
     }
   }
   addProduct(p: Product, image: File) {
-    this.allProducts.push(p);
-    this.saveProducts();
+    this.allProducts().push(p);
   }
   updateProduct(p: Product, image: File) {
-    const index = this.allProducts.findIndex(pr => pr.id == p.id);
-    this.allProducts[index] = p;
-    this.saveProducts();
+    const index = this.allProducts().findIndex(pr => pr.id == p.id);
+    this.allProducts()[index] = p;
   }
   searchProducts(search: string): Product[] {
     const keyword = search.toLowerCase();
-    return this.allProducts.filter(product =>
+    return this.allProducts().filter(product =>
       product.name.toLowerCase().includes(keyword) ||
       product.description.toLowerCase().includes(keyword) ||
       product.brand.toLowerCase().includes(keyword)
@@ -47,10 +50,10 @@ export class ProductsService {
   loadProducts() {
     const products = localStorage.getItem('products');
     if (products) {
-      this.allProducts = JSON.parse(products);
+      this.allProducts.set(JSON.parse(products));
     }
   }
   saveProducts() {
-    localStorage.setItem('products', JSON.stringify(this.allProducts));
+    localStorage.setItem('products', JSON.stringify(this.allProducts()));
   }
 }
