ccb91b6e1303d69d6a3b89ac4f5f80f31aa334bb
Francesco Naletto <46630982+Hisukurifu24@users.noreply.github.com>
2024-11-08 10:40:20 +0100

    Profile page update

--- front-end-prototype/src/app/profile/profile.component.ts
+++ front-end-prototype/src/app/profile/profile.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-import WebApp from '@twa-dev/sdk';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+import WebApp from '@twa-dev/sdk';
 
 
 @Component({
@@ -13,6 +16,7 @@ import { RouterLink } from '@angular/router';
     MatListModule,
     MatIconModule,
     MatButtonModule,
+    MatExpansionModule,
     RouterLink,
   ],
   templateUrl: './profile.component.html',
