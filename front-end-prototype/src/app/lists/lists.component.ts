ffa56cca6592fff9e4602ec066dd24b217f3de15
Francesco Naletto <46630982+Hisukurifu24@users.noreply.github.com>
2024-11-08 10:14:53 +0100

    New list service
    Moved most of lists component code into the service

--- front-end-prototype/src/app/lists/lists.component.ts
+++ front-end-prototype/src/app/lists/lists.component.ts
@@ -1,17 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
-
-import { AmazingList } from './list.model';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { CdkContextMenuTrigger, CdkMenu, CdkMenuItem } from '@angular/cdk/menu';
-import { MatMenuModule } from '@angular/material/menu';
 
-import WebApp from '@twa-dev/sdk';
-import { NewListComponent } from './new-list/new-list.component';
-import { MatDialog } from '@angular/material/dialog';
-import { RouterLink } from '@angular/router';
+import { ListsService } from './lists.service';
 
 
 @Component({
@@ -30,98 +25,24 @@ import { RouterLink } from '@angular/router';
   styleUrl: './lists.component.css'
 })
 export class ListsComponent {
-  readonly dialog = inject(MatDialog);
+  private listsService = inject(ListsService);
 
-
-  lists = signal<AmazingList[]>([]);
-
-  constructor() {
-    WebApp.CloudStorage.getItem('lists', (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        this.lists.set(JSON.parse(data ?? ''));
-      }
-    });
-  }
-
-  editList(id: string) {
-    // Find the list to edit
-    const listToEdit = this.lists().find((list) => list.id === id);
-
-    if (!listToEdit) {
-      console.error('List not found');
-      return;
-    }
-
-    const dialogRef = this.dialog.open(NewListComponent, {
-      data: { name: listToEdit.name, description: listToEdit.description },
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result !== undefined) {
-        const newList: AmazingList = {
-          id: Date.now().toString(),
-          name: result.name,
-          description: result.description,
-          items: [],
-        };
-
-        // Replace the list with the new one
-        this.lists.set(this.lists().map((item) => item.id === id ? newList : item));
-
-        WebApp.CloudStorage.setItem('lists', JSON.stringify(this.lists()), (err) => {
-          if (err) {
-            console.error(err);
-          }
-        });
-      }
-    });
-  }
-
-  removeList(id: string) {
-    if (!confirm('Are you sure you want to delete this list?')) {
-      return;
-    }
-    this.lists.set(this.lists().filter((item) => item.id !== id));
-    WebApp.CloudStorage.setItem('lists', JSON.stringify(this.lists()), (err) => {
-      if (err) {
-        console.error(err);
-      }
-    });
+  get lists() {
+    return this.listsService.lists();
   }
 
   addList() {
-    const dialogRef = this.dialog.open(NewListComponent, {
-      data: { name: '', description: '' },
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result !== undefined) {
-        const newList: AmazingList = {
-          id: Date.now().toString(),
-          name: result.name,
-          description: result.description,
-          items: [],
-        };
-
-        this.lists().push(newList);
-        WebApp.CloudStorage.setItem('lists', JSON.stringify(this.lists()), (err) => {
-          if (err) {
-            console.error(err);
-          }
-        });
-      }
-    });
+    this.listsService.addList();
+  }
+  removeList(arg0: any) {
+    this.listsService.removeList(arg0);
+  }
+  editList(arg0: any) {
+    this.listsService.editList(arg0);
+  }
+  share(_t7: any) {
+    this.listsService.share(_t7);
   }
 
-  share(list: AmazingList) {
-    const url: string = `http://t.me/AmazingSynclabBot/amazing/lists/${list.id}`;
 
-    const text: string = 'Check out this list!\n' + JSON.stringify(list, null, 2);
-
-
-    const link: string = `https://t.me/share/url?url=${url}&text=${text}`;
-    WebApp.openTelegramLink(link);
-  }
 }
